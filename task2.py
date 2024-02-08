bef935c037159c27a71cffe8a6e8b0a37e6b85e9
kettycollins <144057221+kettycollins@users.noreply.github.com>
2024-02-08 14:54:52 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,14 +1,16 @@
 import turtle
+
 def draw_pythagoras_tree(t, branch_len, level):
     if level == 0:
         return
     t.forward(branch_len)
-    t.left(45)
+    t.right(45)  # Змінено лівий поворот на правий
     draw_pythagoras_tree(t, branch_len * 0.7, level - 1)
-    t.right(90)
+    t.left(90)   # Змінено правий поворот на лівий
     draw_pythagoras_tree(t, branch_len * 0.7, level - 1)
-    t.left(45)
+    t.right(45)  # Змінено лівий поворот на правий
     t.backward(branch_len)
+
 def main():
     window = turtle.Screen()
     window.bgcolor("white")
@@ -21,5 +23,6 @@ def main():
     level = int(input("Введіть рівень рекурсії для фракталу: "))
     draw_pythagoras_tree(t, 100, level)
     window.mainloop()
+
 if __name__ == "__main__":
     main()
